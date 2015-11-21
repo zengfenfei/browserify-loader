7083dda38d31c55f6944ada30d4dff034ea3f18a
KevinZeng <zengfenfei@gmail.com>
2015-11-21 22:36:24 +0800

    loader test cases

--- test/loader_tests.js
+++ test/loader_tests.js
@@ -1,4 +1,36 @@
-QUnit.test( "load moule file", function( assert ) {
-  var done = assert.async()
+var loaderCases = [{
+	name: 'load as a file',
+	module: './index.html'
+}, {
+	name: 'load with js extenstion',
+	module: './m'
+}, {
+	name: 'load with json extenstion',
+	module: './obj'
+}, {
+	name: 'load directory without package.json',
+	module: './folder_module'
+}, {
+	name: 'load as directory with package.json',
+	module: './folder_module2'
+}, {
+	name: 'load folder from node_modules with package.json',
+	module: 'fm'
+}, {
+	name: 'load from node_modules',
+	module: 'nm'
+}];
+loaderCases.forEach(testLoaderCase)
 
-})
+function testLoaderCase (c) {
+	QUnit.test(c.name, function (assert) {
+		var done = assert.async();
+		loadModule(c.module, location.pathname).then(function (module) {
+			assert.ok(1, c.module + ' -> '+ module.url)
+			done()
+		}, function (reason) {
+			assert.ok(0, reason)
+			done()
+		})
+	});
+}
